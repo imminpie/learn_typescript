e8c74dee22a73958f0f32e27b1248a1748b63798
imminpie <mjkang0715@gmail.com>
2024-01-01 15:55:54 +0900

    feat: 함수 타입의 호환성

---
+++ onebite-typescript/section4/src/chapter3.ts
@@ -0,0 +1,56 @@
+/**
+ * 함수 타입 호환성
+ */
+
+// 기준 1. 반환 타입의 호환성
+type A = () => number;
+type B = () => 10;
+
+let a: A = () => 10;
+let b: B = () => 10;
+
+a = b;
+// b = a; ❌
+
+// 기준 2. 매개변수 타입의 호환성
+// 예제1
+type C = (value: number) => void;
+type D = (value: 10) => void;
+
+let c: C = (value) => {};
+let d: D = (value) => {};
+
+// c = d; 💩
+d = c;
+
+// 예제2
+type Animal = {
+  name: string;
+};
+
+type Dog = {
+  name: string;
+  color: string;
+};
+
+let animalFunc = (animal: Animal) => {
+  console.log(animal.name);
+};
+
+let dogFunc = (dog: Dog) => {
+  console.log(dog.name);
+  console.log(dog.color);
+};
+
+// animalFunc = dogFunc; 💩
+dogFunc = animalFunc;
+
+// 예제3
+type Func1 = (a: number, b: number) => void;
+type Func2 = (a: number) => void;
+
+let func1: Func1 = (a, b) => {};
+let func2: Func2 = (a) => {};
+
+func1 = func2;
+// func2 = func1; 💩
