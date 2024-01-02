a332651f8389852a24c801de97ee1fe259490829
imminpie <mjkang0715@gmail.com>
2024-01-02 22:03:36 +0900

    feat: Discriminated Union

---
+++ dreamcoding-typescript/01-TYPES/1-6-discriminated.ts
@@ -0,0 +1,28 @@
+{
+  type SuccessState = {
+    result: 'success';
+    response: {
+      body: string;
+    };
+  };
+
+  type FailState = {
+    result: 'fail';
+    reason: string;
+  };
+
+  type LoginState = SuccessState | FailState;
+
+  function printLoginState(state: LoginState) {
+    if (state.result === 'success') {
+      console.log(`🎉 ${state.response.body}`);
+    } else {
+      console.log(`😭 ${state.reason}`);
+    }
+  }
+
+  printLoginState({
+    result: 'success',
+    response: { body: 'logged in' },
+  });
+}
