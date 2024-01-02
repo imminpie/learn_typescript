4f92ea21aaa41e3dc1538ba6532de10f1d751a09
imminpie <mjkang0715@gmail.com>
2024-01-02 21:41:56 +0900

    feat: 배열과 튜플

---
+++ dreamcoding-typescript/01-TYPES/1-3-array.ts
@@ -0,0 +1,10 @@
+{
+  // Array
+  const numArr: number[] = [1, 2, 3];
+  const strArr: Array<string> = ['Java', 'Spring', 'TypeScript'];
+
+  // Tuple
+  let tup1: [number, number] = [1, 2];
+  let tup2: [number, string, boolean] = [1, '2', true];
+  // tup2 = [false, '3', 3]; 💩 순서 불일치
+}
