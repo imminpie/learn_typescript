a0d48d65a4336b2be06a28386a4cb642e945708b
imminpie <mjkang0715@gmail.com>
2024-01-03 20:45:31 +0900

    feat: 인터페이스, 모든 것의 시작

--- dreamcoding-typescript/03-OOP/3-4-abstraction.ts
+++ dreamcoding-typescript/03-OOP/3-4-abstraction.ts
@@ -12,8 +12,15 @@
     makeCoffee(shots: number): CoffeeCup;
   }
 
+  // CommercialCoffeeMaker 인터페이스 정의
+  interface CommercialCoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+    fillCoffeeBeans(beans: number): void;
+    clean(): void;
+  }
+
   // CoffeeMaker 클래스 정의
-  class CoffeeMachine implements CoffeeMaker {
+  class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
     // 에스프레소 한 샷을 만들 때 필요한 커피 원두 양 (그램 단위)
     private static BEANS_GRAM_PER_SHOT: number = 7;
     private coffeeBeans: number = 0;
@@ -37,6 +44,10 @@
         throw new Error(`😢 value for beans should be greater than 0`);
       }
       this.coffeeBeans += beans;
+    }
+
+    clean() {
+      console.log(`cleaning the machine...🧽`);
     }
 
     // 원두 갈기
@@ -86,7 +97,53 @@
 
   // CoffeeMaker 인터페이스를 통해 CoffeeMachine 클래스의 인스턴스 생성
   const maker2: CoffeeMaker = CoffeeMachine.makeMachine(32);
-  // makeCoffee 메서드를 호출하여 커피 생성
   maker2.makeCoffee(2);
   // maker2.fillCoffeeBeans(32); 💩
+
+  // CommercialCoffeeMaker 인터페이스를 통해 CoffeeMachine 클래스의 인스턴스 생성
+  const maker3: CommercialCoffeeMaker = CoffeeMachine.makeMachine(32);
+  maker3.fillCoffeeBeans(32);
+  maker3.makeCoffee(2);
+  maker3.clean();
+
+  // 예제
+  // AmateurUser 클래스 정의
+  class AmateurUser {
+    // 클래스 내에서 machine 변수를 사용하여
+    // CoffeeMaker 인스턴스에 정의된 함수를 호출할 수 있다.
+    private machine: CoffeeMaker;
+
+    constructor(machine: CoffeeMaker) {
+      this.machine = machine;
+    }
+
+    makeAmateurUserCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+    }
+  }
+
+  // ProBarista 클래스 정의
+  class ProBarista {
+    // 클래스 내에서 machine 변수를 사용하여
+    // CommercialCoffeeMaker 인스턴스에 정의된 함수를 호출할 수 있다.
+    constructor(private machine: CommercialCoffeeMaker) {}
+    makeProBaristaCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+      this.machine.fillCoffeeBeans(32);
+      this.machine.clean();
+    }
+  }
+
+  // CoffeeMachine 클래스의 인스턴스 생성
+  const maker4: CoffeeMachine = CoffeeMachine.makeMachine(32);
+
+  // AmateurUser 클래스의 인스턴스 생성 및 makeAmateurUserCoffee 메서드 호출
+  const amateur = new AmateurUser(maker4);
+  amateur.makeAmateurUserCoffee();
+
+  // ProBarista 클래스의 인스턴스 생성 및 makeProBaristaCoffee 메서드 호출
+  const pro = new ProBarista(maker4);
+  pro.makeProBaristaCoffee();
 }
