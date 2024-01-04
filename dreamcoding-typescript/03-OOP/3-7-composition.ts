9a0de5cd84aa4ffe083e910f28fd32a82cbfa4b8
imminpie <mjkang0715@gmail.com>
2024-01-04 17:09:13 +0900

    feat: 강력한 Interface

--- dreamcoding-typescript/03-OOP/3-7-composition.ts
+++ dreamcoding-typescript/03-OOP/3-7-composition.ts
@@ -1,8 +1,7 @@
 {
   /**
-   * 상속의 문제점
-   * 상속의 깊이가 깊어질수록 서로의 관계가 복잡해진다.
-   * => 상속 대신 구성을 사용한다.
+   * 강력한 Interface
+   * 클래스 대신 Interface 를 통해서 의사소통을 한다.
    */
 
   type CoffeeCup = {
@@ -16,8 +15,20 @@
     makeCoffee(shots: number): CoffeeCup;
   }
 
-  // 싸구려 우유 거품기 클래스 정의
-  class CheapMilkSteamer {
+  // 우유 거품기 인터페이스
+  interface MilkFrother {
+    // 주어진 커피컵에 대해 우유 거품을 만드는 메서드 정의
+    makeMilk(cup: CoffeeCup): CoffeeCup;
+  }
+
+  // 설탕 제조기 인터페이스
+  interface SugarProvider {
+    // 주어진 커피컵에 설탕을 추가하는 메서드 정의
+    addSugar(cup: CoffeeCup): CoffeeCup;
+  }
+
+  // 우유 거품기
+  class CheapMilkSteamer implements MilkFrother {
     // 우유를 스팀하기
     private steamMilk(): void {
       console.log(`Steaming some milk...🥛`);
@@ -33,8 +44,46 @@
     }
   }
 
-  // 설탕 제조기 클래스 정의
-  class CandySugarMixer {
+  class FancyMilkSteamer implements MilkFrother {
+    // 우유를 스팀하기
+    private steamMilk(): void {
+      console.log(`Steaming some milk...🥛`);
+    }
+
+    // 우유를 추가하는 메서드
+    makeMilk(cup: CoffeeCup): CoffeeCup {
+      this.steamMilk();
+      return {
+        ...cup,
+        hasMilk: true,
+      };
+    }
+  }
+
+  class ColdMilkSteamer implements MilkFrother {
+    // 우유를 스팀하기
+    private steamMilk(): void {
+      console.log(`Steaming some milk...🥛`);
+    }
+
+    // 우유를 추가하는 메서드
+    makeMilk(cup: CoffeeCup): CoffeeCup {
+      this.steamMilk();
+      return {
+        ...cup,
+        hasMilk: true,
+      };
+    }
+  }
+
+  class NoMilk implements MilkFrother {
+    makeMilk(cup: CoffeeCup): CoffeeCup {
+      return cup;
+    }
+  }
+
+  // 설탕 제조기
+  class CandySugarMixer implements SugarProvider {
     // 설탕을 가져오기
     private getSugar(): boolean {
       console.log(`Getting some sugar from 🍭`);
@@ -51,6 +100,29 @@
     }
   }
 
+  class SugarMixer implements SugarProvider {
+    // 설탕을 가져오기
+    private getSugar(): boolean {
+      console.log(`Getting some sugar from jar`);
+      return true;
+    }
+
+    // 설탕을 추가하는 메서드
+    addSugar(cup: CoffeeCup): CoffeeCup {
+      const sugar = this.getSugar();
+      return {
+        ...cup,
+        hasSugar: sugar,
+      };
+    }
+  }
+
+  class NoSugar implements SugarProvider {
+    addSugar(cup: CoffeeCup): CoffeeCup {
+      return cup;
+    }
+  }
+
   // 커피 머신 클래스 정의
   class CoffeeMachine implements CoffeeMaker {
     // 에스프레소 한 샷을 만들 때 필요한 커피 원두 양 (그램 단위)
@@ -61,14 +133,12 @@
     // private 으로 선언된 생성자는 일반적으로 외부에서 클래스의 인스턴스를 직접 생성하는 것을 막고,
     // 대신 클래스 내부에서만 인스턴스를 생성할 수 있도록 한다.
     // 일단 에러 때문에 public 으로 수정함, 보통은 private 으로 설정해줌
-    public constructor(coffeeBeans: number) {
+    public constructor(
+      coffeeBeans: number,
+      private milk: MilkFrother,
+      private sugar: SugarProvider
+    ) {
       this.coffeeBeans = coffeeBeans;
-    }
-
-    // CoffeeMaker 인스턴스를 생성하는 정적 메서드
-    // makeMachine 메서드를 통해 CoffeeMaker 클래스의 인스턴스를 생성하도록 한다.
-    static makeMachine(coffeeBeans: number): CoffeeMachine {
-      return new CoffeeMachine(coffeeBeans);
     }
 
     // 커피 원두를 보충하는 메서드
@@ -114,63 +184,28 @@
     makeCoffee(shots: number): CoffeeCup {
       this.grindBeans(shots);
       this.preheat();
-      return this.extract(shots);
-    }
-  }
-
-  // CafeLatteMachine 클래스 정의
-  class CafeLatteMachine extends CoffeeMachine {
-    // 생성자에서 우유 거품기를 받아옴
-    constructor(beans: number, private milkFrother: CheapMilkSteamer) {
-      // 자바스크립트에서 클래스가 다른 클래스를 상속할 때,
-      // 자식 클래스의 생성자에서 반드시 부모 클래스의 생성자를 호출해야 한다.
-      super(beans);
-    }
-
-    // 부모 클래스의 makeCoffee 를 재정의하여 우유 스팀 동작을 추가함
-    makeCoffee(shots: number): CoffeeCup {
-      // super 키워드를 사용해서 부모 클래스의 메서드를 호출함
-      const coffee = super.makeCoffee(shots);
-      // 우유 스팀 동작을 수행
-      return this.milkFrother.makeMilk(coffee);
-    }
-  }
-
-  // SweetCoffeeMaker 클래스 정의
-  class SweetCoffeeMaker extends CoffeeMachine {
-    // 생성자에서 설탕 제조기를 받아옴
-    constructor(beans: number, private sugar: CandySugarMixer) {
-      // 자바스크립트에서 클래스가 다른 클래스를 상속할 때,
-      // 자식 클래스의 생성자에서 반드시 부모 클래스의 생성자를 호출해야 한다.
-      super(beans);
-    }
-
-    // 부모 클래스의 makeCoffee 메서드를 재정의하여 설탕 추가 동작을 추가함
-    makeCoffee(shots: number): CoffeeCup {
-      const coffee = super.makeCoffee(2);
-      // 설탕 추가 동작을 수행
-      return this.sugar.addSugar(coffee);
-    }
-  }
-
-  // SweetCafeLatteMachine 클래스 정의
-  class SweetCafeLatteMachine extends CoffeeMachine {
-    // 생성자에서 우유 거품기와 설탕 제조기를 받아옴
-    constructor(
-      beans: number,
-      private milkFrother: CheapMilkSteamer,
-      private sugar: CandySugarMixer
-    ) {
-      // 자바스크립트에서 클래스가 다른 클래스를 상속할 때,
-      // 자식 클래스의 생성자에서 반드시 부모 클래스의 생성자를 호출해야 한다.
-      super(beans);
-    }
-
-    // 부모 클래스의 makeCoffee 메서드를 재정의하여 설탕 추가 동작을 추가함
-    makeCoffee(shots: number): CoffeeCup {
-      const coffee = super.makeCoffee(2);
+      const coffee = this.extract(shots);
       const sugarAdded = this.sugar.addSugar(coffee);
-      return this.milkFrother.makeMilk(sugarAdded);
+      return this.milk.makeMilk(sugarAdded);
     }
   }
+
+  // Milk
+  const cheapMilkMaker = new CheapMilkSteamer();
+  const fancyMilkMaker = new FancyMilkSteamer();
+  const coldMilkFrother = new ColdMilkSteamer();
+  const noMilk = new NoMilk();
+
+  // Sugar
+  const candySugar = new CandySugarMixer();
+  const sugar = new SugarMixer();
+  const noSugar = new NoSugar();
+
+  // Machine
+  const sweetCandyMachine = new CoffeeMachine(12, noMilk, candySugar);
+  const sweetMachine = new CoffeeMachine(12, noMilk, sugar);
+
+  const latteMachine = new CoffeeMachine(12, cheapMilkMaker, noSugar);
+  const coldLatteMachine = new CoffeeMachine(12, coldMilkFrother, noSugar);
+  const sweetLatteMachine = new CoffeeMachine(12, cheapMilkMaker, candySugar);
 }
