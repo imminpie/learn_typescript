7d6f2920447aeadcc989b1313adefc737710b962
imminpie <mjkang0715@gmail.com>
2024-01-07 15:40:43 +0900

    feat: 모듈 데모

---
+++ dreamcoding-typescript/10-JAVASCRIPT/10-3-module1.js
@@ -0,0 +1,6 @@
+export default function add(a, b) {
+  return a + b;
+}
+
+// default 는 한 파일 안에서 하나만 가능하다.
+export function print() {}
---
+++ dreamcoding-typescript/10-JAVASCRIPT/10-3-module2.js
@@ -0,0 +1,7 @@
+// console.log(add(1, 2)); Script 태그에 Module 타입을 정의하지 않았기 때문이다. 💩
+
+import add, { print } from './10-3-module1.js';
+
+console.log(add(1, 2)); // 3 🙆‍♀️
+
+print();
