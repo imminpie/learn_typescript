158be44a8847d992f0e10d5112a6533fba79304c
imminpie <mjkang0715@gmail.com>
2024-01-25 14:10:40 +0900

    feat: Any 와 Unknown 타입

---
+++ onebite-typescript/section02/src/chapter6.ts
@@ -0,0 +1,13 @@
+// any 💩
+let anyVar: any = 10;
+anyVar = 'Hello!';
+
+anyVar = true;
+anyVar = {};
+anyVar = () => {};
+
+// Unknown
+let unknownVar: unknown;
+unknownVar = '';
+unknownVar = 1;
+unknownVar = () => {};
